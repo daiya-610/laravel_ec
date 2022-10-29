f679dd20214b11dcd8be9df1a08ea16554c248ea
DaiyaFujimoto <fujimotod03@gmail.com>
2022-10-29 23:12:30 +0900

    ソフトデリート処理

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -70,7 +70,8 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー登録を実施しました。');
+        ->with(['message'=> 'オーナー登録を実施しました。',
+        'status' => 'info']);
     }
 
     /**
@@ -114,7 +115,8 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー情報を更新しました。');
+        ->with(['message'=> 'オーナー情報を更新しました。',
+        'status' => 'info']);
     }
 
     /**
@@ -125,6 +127,11 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        dd('削除処理');
+        Owner::findOrFail($id)->delete(); //ソフトデリート
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message'=> 'オーナー情報を削除しました。',
+        'status' => 'alert']);
     }
 }
